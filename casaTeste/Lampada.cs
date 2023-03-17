3e934411ab4224dfcd8819adeddc3d70b8a18b6d
João Vitor Delaia <115042912+M4ntov4@users.noreply.github.com>
2023-03-16 21:39:51 -0300

    Add files via upload

---
+++ casaTeste/Lampada.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casaTeste
+{
+    class Lampada
+    {
+        bool luz;
+        string potencia;
+        string voltagem;
+        
+
+        public bool Luz { get => luz; set => luz = value; }//crtl + R + E
+        public string Potencia { get => potencia; set => potencia = value; }
+        public string Voltagem { get => voltagem; set => voltagem = value; }
+
+        public Lampada()
+        {
+            this.Luz = Luz;
+            this.Potencia = "60w";
+            this.Voltagem = "127v";
+        }
+
+        public bool Queimou()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(100);
+
+            if (numeroAleatorio < 15)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+
+            }
+
+        }
+
+        public void Ascender()
+        {
+            if (Queimou())
+            {
+                this.Luz = false;
+            }
+            else
+            {
+                this.Luz = true;
+            }
+        }
+
+
+        public void Apagar()
+        {
+            this.Luz = false;
+        }
+
+        public bool LerStatus()
+        {
+            return this.Luz;
+        }
+
+        public string MostrarVoltagem()
+        {
+            return this.Voltagem;
+
+        }
+
+        public string MostrarPotencia()
+        {
+            return this.Potencia;
+        }
+
+        public void AlterarVoltagem(string valor)
+        {
+            this.Voltagem = valor;          
+        }
+        
+        public void AlterarPotencia(string valor)
+        {
+            this.Potencia = valor;   
+        }
+
+
+
+
+
+    }
+}
---
+++ casaTeste/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lampada lampi = new Lampada();
+
+            //Console.WriteLine("A lampada está: " + lampi.LerStatus());
+            
+
+            Console.Write("Altere a potencia da Lampada: ");
+            lampi.AlterarPotencia(Console.ReadLine());
+            Console.Write("Altere a Voltagem da Lampada: ");
+            lampi.AlterarVoltagem(Console.ReadLine());
+            Console.WriteLine("A Voltagem é: " + lampi.MostrarVoltagem());
+            Console.WriteLine("A potencia é: " + lampi.MostrarPotencia());
+            lampi.Apagar();
+            lampi.Ascender();
+
+            Console.WriteLine(lampi.LerStatus());
+
+            Console.ReadKey();
+
+            
+            
+
+
+        }
+    }
+}
